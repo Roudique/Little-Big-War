6684551f6452859573e5f87d08fb802d9aee2caf
roudique <roudique@gmail.com>
2015-10-05 14:32:43 +0300

    added utilities class

---
+++ src/com/roudique/lbw/utils/Utilities.java
@@ -0,0 +1,9 @@
+package com.roudique.lbw.utils;
+
+public class Utilities {
+	public static String[] getUnitList() {
+		String[] unitList = {"Berserk", "Demon", "Healer", "Necromancer", "Priest",
+								"Rogue", "Vampire", "Warlock", "Werewolf", "Wizard"};
+		return unitList;
+	}
+}
